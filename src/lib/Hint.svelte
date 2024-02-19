c91822f07bd20540f96ded7bc9b587cd7860a840
Simon Murray <spjmurray@yahoo.co.uk>
2024-02-19 09:39:20 +0000

    Tweak Styles Again
    Fresh eyes after holiday have more taste evidently!

--- src/lib/Hint.svelte
+++ src/lib/Hint.svelte
@@ -1,4 +1,4 @@
-<section>
+<section class="overlay">
 	<iconify-icon icon="tabler:bulb" />
 	<div>
 		<slot />
@@ -10,11 +10,7 @@
 		display: flex;
 		gap: var(--padding);
 		align-items: center;
-		background: var(--overlay);
-		backdrop-filter: blur(var(--radius));
-		border-radius: var(--radius);
 		padding: var(--padding-small);
-		box-shadow: 0 0 var(--radius) var(--shadow);
 	}
 	iconify-icon {
 		color: orange;
--- src/lib/Item.svelte
+++ src/lib/Item.svelte
@@ -10,7 +10,7 @@
 	}
 </script>
 
-<article on:click={click} on:keypress={click} class:selected>
+<article on:click={click} on:keypress={click} class="overlay" class:overlay-selected={selected}>
 	<header>
 		<slot name="header" />
 	</header>
@@ -20,16 +20,6 @@
 </article>
 
 <style>
-	article {
-		cursor: pointer;
-		border-radius: var(--radius);
-		background-color: var(--overlay);
-		backdrop-filter: blur(var(--radius));
-		box-shadow: 0 0 var(--radius) var(--shadow);
-	}
-	article.selected {
-		background-color: var(--overlay-highlight);
-	}
 	header {
 		transition: all 0.3s ease-in-out;
 		padding: var(--padding);
