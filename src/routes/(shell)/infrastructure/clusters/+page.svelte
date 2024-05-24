8f19287abeb1ca5a8650a02fb71834693fb3831d
Simon Murray <spjmurray@yahoo.co.uk>
2024-05-24 12:49:47 +0100

    Allow Kubeconfig Downloads

--- src/routes/(shell)/infrastructure/clusters/+page.svelte
+++ src/routes/(shell)/infrastructure/clusters/+page.svelte
@@ -20,13 +20,15 @@
 	import { organizationStore } from '$lib/stores';
 
 	/* Client setup */
-	import { client, error } from '$lib/clients';
+	import * as Clients from '$lib/clients';
 	import type { InternalToken } from '$lib/oauth2';
 	import { token } from '$lib/credentials';
 	import * as Models from '$lib/openapi/server/models';
 
 	import { getToastStore } from '@skeletonlabs/skeleton';
 	const toastStore = getToastStore();
+
+	import { browser } from '$app/environment';
 
 	let at: InternalToken;
 
@@ -51,13 +53,13 @@
 		if (!at || !organization) return;
 
 		const parameters = {
-			organizationName: organization
+			organization: organization
 		};
 
-		client(toastStore, at)
-			.apiV1OrganizationsOrganizationNameClustersGet(parameters)
+		Clients.client(toastStore, at)
+			.apiV1OrganizationsOrganizationClustersGet(parameters)
 			.then((v: Models.KubernetesClusters) => (resources = v))
-			.catch((e: Error) => error(e));
+			.catch((e: Error) => Clients.error(e));
 	}
 
 	function remove(resource: Models.KubernetesCluster): void {
@@ -70,20 +72,46 @@
 				if (!resource.metadata) return;
 
 				const parameters = {
-					organizationName: organization,
-					projectName: resource.metadata.project,
-					clusterName: resource.spec.name
+					organization: organization,
+					project: resource.metadata.project,
+					cluster: resource.spec.name
 				};
 
-				client(toastStore, at)
-					.apiV1OrganizationsOrganizationNameProjectsProjectNameClustersClusterNameDelete(
-						parameters
-					)
-					.catch((e: Error) => error(e));
+				Clients.client(toastStore, at)
+					.apiV1OrganizationsOrganizationProjectsProjectClustersClusterDelete(parameters)
+					.catch((e: Error) => Clients.error(e));
 			}
 		};
 
 		modalStore.trigger(modal);
+	}
+
+	function getKubeconfig(resource: Models.KubernetesCluster): void {
+		const parameters = {
+			organization: organization,
+			project: resource.metadata.project,
+			cluster: resource.spec.name
+		};
+
+		Clients.client(toastStore, at)
+			.apiV1OrganizationsOrganizationProjectsProjectClustersClusterKubeconfigGetRaw(parameters)
+			.then((x) => x.raw.blob())
+			.then((x) => {
+				if (browser) {
+					const url = window.URL.createObjectURL(x);
+
+					const a = document.createElement('a');
+					a.style.display = 'none';
+					a.href = url;
+					a.download = `kubeconfig-${resource.spec.name}.yaml`;
+
+					document.body.appendChild(a);
+					a.click();
+
+					window.URL.revokeObjectURL(url);
+				}
+			})
+			.catch((e: Error) => Clients.error(e));
 	}
 
 	import StatusIcon from '$lib/StatusIcon.svelte';
@@ -106,9 +134,17 @@
 					<h6 class="h6">{resource.spec.name}</h6>
 				</header>
 
-				<button on:click={() => remove(resource)} on:keypress={() => remove(resource)}>
-					<iconify-icon icon="mdi:close" />
-				</button>
+				<div class="flex items-center gap-4">
+					<button
+						on:click={() => getKubeconfig(resource)}
+						on:keypress={() => getKubeconfig(resource)}
+					>
+						<iconify-icon icon="mdi:file-document-outline" />
+					</button>
+					<button on:click={() => remove(resource)} on:keypress={() => remove(resource)}>
+						<iconify-icon icon="mdi:close" />
+					</button>
+				</div>
 			</ShellListItem>
 		{/each}
 	</ShellList>
