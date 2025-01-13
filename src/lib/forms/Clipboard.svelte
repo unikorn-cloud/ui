706c6d7dc0cf09ce09b05da0dfe2ae7a001061c1
Simon Murray <spjmurray@yahoo.co.uk>
2025-01-13 12:19:27 +0000

    Remove Clipboard (#111)
    Removed in skeleton 3, plus there is a native API to use in the browser
    now.

---
+++ src/lib/forms/Clipboard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	interface Props {
+		id: string;
+		value: string;
+	}
+
+	let { id, value }: Props = $props();
+
+	function copyToClipboard() {
+		const input = document.getElementById(id) as HTMLInputElement;
+		if (!input) return;
+
+		navigator.clipboard.writeText(input.value);
+	}
+</script>
+
+<div class="input-group input-group-divider grid-cols-[1fr_auto]">
+	<input
+		{id}
+		{value}
+		type="text"
+		disabled
+		class="overflow-hidden text-ellipsis whitespace-nowrap"
+	/>
+	<button
+		class="variant-filled-primary flex items-center gap-2"
+		onclick={() => copyToClipboard()}
+		onkeypress={() => copyToClipboard()}
+	>
+		<iconify-icon icon="mdi:clipboard-outline"></iconify-icon>
+		<span>Copy</span>
+	</button>
+</div>
