f9b7d4a4299ceaa324bf28afacaa16566bb8998f
Simon Murray <spjmurray@yahoo.co.uk>
2025-01-23 10:25:54 +0000

    Add an Error Page
    This can be redirected to by identity, for example, so it can display
    branched error messages in the corrct client e.g. during authentication
    or as a result of user verification work flows.

---
+++ src/routes/error/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	import ErrorPage from '$lib/layouts/ErrorPage.svelte';
+</script>
+
+<ErrorPage
+	error={page.url.searchParams.get('error') || undefined}
+	message={page.url.searchParams.get('message') || undefined}
+/>
