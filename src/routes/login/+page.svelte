d4de901dbbea5f8fcd9b0679ec85c15995fde1d7
Simon Murray <spjmurray@yahoo.co.uk>
2025-01-24 10:59:06 +0000

    Make Providers Dynamic

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
+	import type { PageData } from './$types';
 
+	let { data }: { data: PageData } = $props();
+
+	import * as Identity from '$lib/openapi/identity';
 	import Logo from '$lib/logos/Logo.svelte';
 
-	// POST address from the authention server.
-	let callback: string = $state('#');
-	// State from the authentication server.
-	let serverState: string = $state('undefined');
-
-	// Get state from the authentication server.
-	if (browser) {
-		const location = new URL(window.location.href);
-
-		if (location.searchParams.has('callback')) {
-			callback = location.searchParams.get('callback') || 'undefined';
-		}
-
-		if (location.searchParams.has('state')) {
-			serverState = location.searchParams.get('state') || 'undefined';
-		}
-	}
-
-	function login(provider: string) {
+	function login(provider: Identity.Oauth2ProviderType) {
 		const providerInput = document.getElementById('provider') as HTMLInputElement;
 		if (providerInput) {
 			providerInput.value = provider;
@@ -32,18 +17,6 @@
 			form.submit();
 		}
 	}
-
-	function google() {
-		login('google');
-	}
-
-	function microsoft() {
-		login('microsoft');
-	}
-
-	function github() {
-		login('github');
-	}
 </script>
 
 <div class="grid grid-cols-[1fr_max-content_1fr] grid-rows-[1fr_max-content_1fr] w-full h-screen">
@@ -53,8 +26,8 @@
 		</header>
 
 		<main class="flex flex-col gap-4 items-center">
-			<form id="login_form" class="flex flex-col gap-8" method="post" action={callback}>
-				<input name="state" type="hidden" value={serverState} />
+			<form id="login_form" class="flex flex-col gap-8" method="post" action={data.callback}>
+				<input name="state" type="hidden" value={data.state} />
 				<input id="provider" name="provider" type="hidden" />
 
 				<section class="flex flex-col gap-4 items-center">
@@ -77,30 +50,36 @@
 				<p>Choose a generic provider</p>
 
 				<div class="flex justify-center gap-4">
-					<button
-						class="btn variant-ghost-surface shadow-lg flex align-center"
-						onclick={google}
-						onkeypress={google}
-						aria-label="Google"
-					>
-						<iconify-icon icon="logos:google-icon"></iconify-icon>
-					</button>
-					<button
-						class="btn variant-ghost-surface shadow-lg flex align-center"
-						onclick={microsoft}
-						onkeypress={microsoft}
-						aria-label="Mircosoft"
-					>
-						<iconify-icon icon="logos:microsoft-icon"></iconify-icon>
-					</button>
-					<button
-						class="btn variant-ghost-surface shadow-lg flex align-center"
-						onclick={github}
-						onkeypress={github}
-						aria-label="GitHub"
-					>
-						<iconify-icon icon="logos:github-icon"></iconify-icon>
-					</button>
+					{#if data.providers.includes(Identity.Oauth2ProviderType.Google)}
+						<button
+							class="btn variant-ghost-surface shadow-lg flex align-center"
+							onclick={() => login(Identity.Oauth2ProviderType.Google)}
+							onkeypress={() => login(Identity.Oauth2ProviderType.Google)}
+							aria-label="Google"
+						>
+							<iconify-icon icon="logos:google-icon"></iconify-icon>
+						</button>
+					{/if}
+					{#if data.providers.includes(Identity.Oauth2ProviderType.Microsoft)}
+						<button
+							class="btn variant-ghost-surface shadow-lg flex align-center"
+							onclick={() => login(Identity.Oauth2ProviderType.Microsoft)}
+							onkeypress={() => login(Identity.Oauth2ProviderType.Microsoft)}
+							aria-label="Mircosoft"
+						>
+							<iconify-icon icon="logos:microsoft-icon"></iconify-icon>
+						</button>
+					{/if}
+					{#if data.providers.includes(Identity.Oauth2ProviderType.Github)}
+						<button
+							class="btn variant-ghost-surface shadow-lg flex align-center"
+							onclick={() => login(Identity.Oauth2ProviderType.Github)}
+							onkeypress={() => login(Identity.Oauth2ProviderType.Github)}
+							aria-label="GitHub"
+						>
+							<iconify-icon icon="logos:github-icon"></iconify-icon>
+						</button>
+					{/if}
 				</div>
 			</section>
 		</main>
