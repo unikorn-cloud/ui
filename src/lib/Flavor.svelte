f869e2e266860094573ac24bd9f2df7f681a1b4e
Simon Murray <spjmurray@yahoo.co.uk>
2025-02-04 10:11:05 +0000

    Fix Large Flavors on Mobile
    Handles overflow on mobile devices for things with GPUs primarily.
    
    Fixes #145.

--- src/lib/Flavor.svelte
+++ src/lib/Flavor.svelte
@@ -9,27 +9,33 @@
 </script>
 
 {#if flavor}
-	<div class="flex items-center gap-6">
+	<div class="flex flex-col lg:flex-row lg:items-center gap-6">
 		<div class="flex items-center gap-2">
 			<iconify-icon icon="mdi:server-outline" class="text-2xl"></iconify-icon>
-			<div class="flex flex-col items-start">
-				{flavor.spec.cpus} core, {flavor.spec.memory} GiB RAM
-				<div class="text-xs">
-					{flavor.spec.disk} GiB Storage
+			<div class="flex flex-col lg:flex-row gap-2 lg:gap-4">
+				<div class="flex flex-col items-start">
+					{flavor.spec.cpus} core, {flavor.spec.memory} GiB RAM
+					<div class="text-xs">
+						{flavor.spec.disk} GiB Storage
+					</div>
 				</div>
+				{#if flavor.spec.gpu}
+					<div class="flex flex-col items-start">
+						<div class="flex gap-2 items-center">
+							{flavor.spec.gpu.physicalCount}
+							{#if flavor.spec.gpu.vendor == 'AMD'}
+								<iconify-icon icon="logos:amd"></iconify-icon>
+							{:else if flavor.spec.gpu.vendor == 'NVIDIA'}
+								<iconify-icon icon="logos:nvidia"></iconify-icon>
+							{/if}
+							{flavor.spec.gpu.model}
+						</div>
+						<div class="text-xs">
+							{flavor.spec.gpu.logicalCount} core {flavor.spec.gpu.memory} GiB
+						</div>
+					</div>
+				{/if}
 			</div>
 		</div>
-		{#if flavor.spec.gpu}
-			<div class="flex items-center gap-1">
-				{flavor.spec.gpu.physicalCount}
-				{#if flavor.spec.gpu.vendor == 'AMD'}
-					<iconify-icon icon="logos:amd"></iconify-icon>
-				{:else if flavor.spec.gpu.vendor == 'NVIDIA'}
-					<iconify-icon icon="logos:nvidia"></iconify-icon>
-				{/if}
-				{flavor.spec.gpu.model}
-				({flavor.spec.gpu.logicalCount} core {flavor.spec.gpu.memory} GiB)
-			</div>
-		{/if}
 	</div>
 {/if}
