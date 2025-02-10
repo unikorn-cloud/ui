41652f1aebc6e248500b0b7461504875534ad107
Simon Murray <spjmurray@yahoo.co.uk>
2025-02-10 15:14:01 +0000

    Fix Cluster manager Selection

--- src/routes/(shell)/kubernetes/clusters/create/+page.ts
+++ src/routes/(shell)/kubernetes/clusters/create/+page.ts
@@ -8,13 +8,6 @@ import * as Clients from '$lib/clients';
 export const load: PageLoad = async ({ fetch, parent, url }) => {
 	const { token, organizationID } = await parent();
 
-	const clustermanagers = Clients.kubernetes(
-		token,
-		fetch
-	).apiV1OrganizationsOrganizationIDClustermanagersGet({
-		organizationID: organizationID
-	});
-
 	const regionID = url.searchParams.get('regionID');
 	if (!regionID) {
 		error(400, 'region ID not in query');
@@ -37,7 +30,6 @@ export const load: PageLoad = async ({ fetch, parent, url }) => {
 	});
 
 	return {
-		clustermanagers: await clustermanagers,
 		images: await images,
 		flavors: await flavors
 	};
