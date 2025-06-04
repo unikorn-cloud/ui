69805b08783fd20d3b5302c3a08509c7ea095b32
Simon Murray <spjmurray@yahoo.co.uk>
2025-06-04 16:37:45 +0100

    Support Machine Health Status (#238)
    This helps us to pin point what's actually broken, rather than seeing a
    cluster on the whole reporting a degraded status.

--- src/lib/healthStatus/index.ts
+++ src/lib/healthStatus/index.ts
@@ -1,7 +1,7 @@
 import * as Identity from '$lib/openapi/identity';
 
-export function color(meta: Identity.ResourceReadMetadata): string {
-	switch (meta.healthStatus) {
+export function statusColor(s: Identity.ResourceHealthStatus): string {
+	switch (s) {
 		case Identity.ResourceHealthStatus.Healthy:
 			return 'text-success-500';
 		case Identity.ResourceHealthStatus.Degraded:
@@ -10,12 +10,20 @@ export function color(meta: Identity.ResourceReadMetadata): string {
 	return 'dark:text-surface-500';
 }
 
-export function icon(meta: Identity.ResourceReadMetadata): string {
-	switch (meta.healthStatus) {
+export function statusIcon(s: Identity.ResourceHealthStatus): string {
+	switch (s) {
 		case Identity.ResourceHealthStatus.Healthy:
 			return 'mdi:heart-outline';
 		case Identity.ResourceHealthStatus.Degraded:
 			return 'mdi:heart-broken-outline';
 	}
 	return 'mdi:heart-broken-outline';
+}
+
+export function color(meta: Identity.ResourceReadMetadata): string {
+	return statusColor(meta.healthStatus);
+}
+
+export function icon(meta: Identity.ResourceReadMetadata): string {
+	return statusIcon(meta.healthStatus);
 }
