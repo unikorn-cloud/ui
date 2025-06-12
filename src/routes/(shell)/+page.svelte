1e3f26fa05845c8774ee5da827f72b97e863e571
Simon Murray <spjmurray@yahoo.co.uk>
2025-06-12 10:11:11 +0100

    Fix Dashboard View (#244)
    Missed one page when updating the formatting of the header stuff.

--- src/routes/(shell)/+page.svelte
+++ src/routes/(shell)/+page.svelte
@@ -17,44 +17,43 @@
 	};
 </script>
 
-<ShellPageHeader {settings}>
-	<ShellSection title="Resource Utilization">
-		<div class="flex flex-col lg:flex-row lg:flex-wrap gap-4">
-			{#each data.quotas.quotas as quota}
-				<div
-					class="flex flex-col gap-4 card bg-surface-50-950 border border-surface-200-800 shadow p-4"
-				>
-					<div class="flex flex-col gap-2">
-						<div class="h4">{quota.displayName}</div>
-						<div class="italic text-sm text-surface-700-300">{quota.description}</div>
-					</div>
-					<div class="flex items-center gap-4">
-						<!-- the default is to treat the value as a pencentage -->
-						<ProgressRing
-							value={Math.round(quota.quantity <= 0 ? 0 : (quota.used / quota.quantity) * 100)}
-							showLabel
-							trackStroke="stroke-primary-500/20"
-							strokeWidth="0.75rem"
-							labelFontSize={32}
-						></ProgressRing>
+<ShellPageHeader {settings}></ShellPageHeader>
+<ShellSection title="Resource Utilization">
+	<div class="flex flex-col lg:flex-row lg:flex-wrap gap-4">
+		{#each data.quotas.quotas as quota}
+			<div
+				class="flex flex-col gap-4 card bg-surface-50-950 border border-surface-200-800 shadow p-4"
+			>
+				<div class="flex flex-col gap-2">
+					<div class="h4">{quota.displayName}</div>
+					<div class="italic text-sm text-surface-700-300">{quota.description}</div>
+				</div>
+				<div class="flex items-center gap-4">
+					<!-- the default is to treat the value as a pencentage -->
+					<ProgressRing
+						value={Math.round(quota.quantity <= 0 ? 0 : (quota.used / quota.quantity) * 100)}
+						showLabel
+						trackStroke="stroke-primary-500/20"
+						strokeWidth="0.75rem"
+						labelFontSize={32}
+					></ProgressRing>
 
-						<div class="grid grid-cols-[auto_auto] gap-x-4">
-							<div class="font-bold">Total</div>
-							<div>{quota.quantity}</div>
-							<div class="font-bold">Used</div>
-							<div>{quota.used}</div>
-							<div class="font-bold">Free</div>
-							<div>{quota.free}</div>
-							<div class="contents text-surface-700-300">
-								<div class="font-bold">Committed</div>
-								<div>{quota.committed}</div>
-								<div class="font-bold">Reserved</div>
-								<div>{quota.reserved}</div>
-							</div>
+					<div class="grid grid-cols-[auto_auto] gap-x-4">
+						<div class="font-bold">Total</div>
+						<div>{quota.quantity}</div>
+						<div class="font-bold">Used</div>
+						<div>{quota.used}</div>
+						<div class="font-bold">Free</div>
+						<div>{quota.free}</div>
+						<div class="contents text-surface-700-300">
+							<div class="font-bold">Committed</div>
+							<div>{quota.committed}</div>
+							<div class="font-bold">Reserved</div>
+							<div>{quota.reserved}</div>
 						</div>
 					</div>
 				</div>
-			{/each}
-		</div>
-	</ShellSection>
-</ShellPageHeader>
+			</div>
+		{/each}
+	</div>
+</ShellSection>
